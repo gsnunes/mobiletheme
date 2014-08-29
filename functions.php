9fd7859fcf9d8c7529e79a526a94164c153eb276
Guilherme Nunes <guinunesilva@gmail.com>
2014-08-29 15:12:09 -0300

    chmod to less

--- functions.php
+++ functions.php
@@ -60,6 +60,8 @@ function theme_front_page_settings () {
 			"baseColor" => $color_options
 		));
 
+		chmod(get_template_directory() . "/assets/css/main.min.css", 0777);
+
 		$less->compileFile(get_template_directory() . "/assets/less/app.less", get_template_directory() . "/assets/css/main.min.css");
 
 		?>
